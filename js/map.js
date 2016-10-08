44479f73d033fc262d841aaa9af4dcc8d361f802
ice1 <ice1@163.com>
2016-10-08 23:32:26 +0800

    0.1练习网站

---
+++ js/map.js
@@ -0,0 +1,22 @@
+﻿//创建和初始化地图函数：
+function initMap(){
+      createMap();//创建地图
+      setMapEvent();//设置地图事件
+    }
+    function createMap(){ 
+      map = new BMap.Map("map"); 
+      map.centerAndZoom(new BMap.Point(123.41536, 41.783335), 18)
+    }
+    function setMapEvent(){
+      map.enableScrollWheelZoom();
+      map.enableKeyboard();
+      map.enableDragging();
+      map.enableDoubleClickZoom()
+    }
+    function addClickHandler(target,window){
+      target.addEventListener("click",function(){
+        target.openInfoWindow(window);
+      });
+    }
+    var map;
+      
